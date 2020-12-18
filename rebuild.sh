915b69c2b9c45effb616abc3eed1f8c1e6ea8f7f
Max Romanyuta <xom9ik.code@gmail.com>
2020-12-18 19:03:42 +0200

    feat: add responsiveness

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
 docker-compose build && \
 docker-compose down && \
-docker-compose up
+docker-compose up -d
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Logo } from '../Logo';
 import { Menu } from '../Menu';
 
-export const Header: FC = () => (
-  <header className="header">
-    <Logo />
-    <Menu />
-  </header>
-);
+export const Header: FC = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const handlerClick = () => {
+    setIsMenuOpened((prev) => !prev);
+  };
+
+  return (
+    <header className="header">
+      <Logo isMenuOpened={isMenuOpened} />
+      <Menu
+        onClick={handlerClick}
+        isOpen={isMenuOpened}
+      />
+    </header>
+  );
+};
